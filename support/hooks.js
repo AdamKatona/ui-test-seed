a3b68f5d1d26d67e0035f5cab24176cbf0cbd834
Adam Katona <adam_katona@epam.com>
2020-11-18 18:52:07 +0100

    linking chai expect into the fw

--- support/hooks.js
+++ support/hooks.js
@@ -3,6 +3,7 @@ const { BeforeAll, AfterAll } = require("cucumber");
 BeforeAll(async () => {
     await startDriver();
     await driver.manage().window().maximize();
+    // await driver.get("https://angular.io");
 });
 AfterAll(async () => {
     await driver.quit();
